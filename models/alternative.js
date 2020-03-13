ebe46d80ec8f764a487e797084b6469e10c66f52
Danilo Henrique Cordeiro <dancorde@gmail.com>
2020-03-13 19:43:11 -0300

    Created mongoose alternative schema

---
+++ models/alternative.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const alternativeSchema = new Schema({
+  answer: {
+    type: String,
+    required: true
+  },
+  question: {
+    type: Schema.Types.ObjectId,
+    ref: "Question"
+  }
+});
+
+module.exports = mongoose.model("Alternative", alternativeSchema);
--- models/question.js
+++ models/question.js
@@ -7,26 +7,12 @@ const questionSchema = new Schema({
     type: String,
     required: true
   },
-  answer1: {
-    type: String,
-    required: true
-  },
-  answer2: {
-    type: String,
-    required: true
-  },
-  answer3: {
-    type: String,
-    required: true
-  },
-  answer4: {
-    type: String,
-    required: true
-  },
-  answer5: {
-    type: String,
-    required: true
-  }
+  alternatives: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Alternative"
+    }
+  ]
 });
 
 module.exports = mongoose.model("Question", questionSchema);
